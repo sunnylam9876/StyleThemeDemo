f02868e08f8e6541a97689bc5aff8a94c2b16c1e
Sunny Lam <77874599+sunnylam9876@users.noreply.github.com>
2023-12-08 12:06:13 -0800

    completed

--- app/src/main/java/com/example/stylethemedemo/MainActivity.java
+++ app/src/main/java/com/example/stylethemedemo/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.stylethemedemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    AutoCompleteTextView autoTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        autoTextView = findViewById(R.id.autotextView);
+
+        String[] countries = getResources().getStringArray(R.array.countries_array);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, countries);
+        autoTextView.setAdapter(adapter);
+
     }
 }
